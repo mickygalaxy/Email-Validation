5fea5ce8600be8aa1e0385efade842ee5a4d91c6
mickygalaxy <micky27sahu@gmail.com>
2022-10-26 05:31:07 +0530

    email validation

---
+++ emailValidation(regEx).py
@@ -0,0 +1,13 @@
+# a - z  abcdef@gmail.com
+# 0 - 9
+# . _ time 1
+# @ time 1
+# . 2,3
+import re
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]{2,3}$"
+user_email = input(' Enter your Email : ')
+
+if re.search(email_condition,user_email):
+    print(" Right Email")
+else:
+    print(" Wrong Email ")    
